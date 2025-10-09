6381b72ff61f015c7ce4eb0a76b86b0fec0386d9
fahmimik <fahmimik22@gmail.com>
2025-10-09 17:13:29 +0700

    basic array

---
+++ vid34Array.js
@@ -0,0 +1,4 @@
+//vid 34 Array
+//menampilkan angka 5 pada console
+let myArr = ['teks', 2, true, {nama: 'dimas'}, [4,5,6]];
+console.log(myArr[4][1]); //menampilkan index ke 1 pada array di index ke 4
