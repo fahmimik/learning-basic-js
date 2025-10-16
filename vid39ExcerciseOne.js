353feacd165bbf828474e28aa1ec80ef75128989
fahmimik <fahmimik22@gmail.com>
2025-10-16 14:10:28 +0700

    penambahan penghapusan pada list

--- vid39ExcerciseOne.js
+++ vid39ExcerciseOne.js
@@ -1,6 +1,6 @@
 //Passangers Management
-// a. Passangers board
-// a.1 addlist()
+// a. addPassenger
+// a.1 addPassenger()
     // listName
     // array list
         //Rules:
@@ -9,8 +9,6 @@
         // 3. jika ada kursi kosong (karena penumpang turun), maka penumpang yang naik berikutnya duduk dikursi kosong terlebih dahulu
         // 4. asumsi kursi tidak akan pernah penuh dan akan bertambah terus jika ada penumpang naik
         // 5. nama penumpang tidak boleh sama, untuk menghindari kebingungan ketika nanti penumpang turun 
-// b. list get off
-// b.1 deletelist()
 
 let list = [];
 // let inputName = prompt('masukkan nama: ');
@@ -34,13 +32,13 @@ let addPassenger = function(passengerName, list){
     // jika ada nama yang sama
     else if(list[i] === passengerName){
         // tampilkan pesa kesalahannya
-        console.log(passengerName+'nama sama');
+        console.log(passengerName+' adalah nama yang sama');
         // kembalikan isi array & keluar dari function
         return list;
     }
 
     // jika seluruh kursi terisi
-    else if (i == list.length - 1) {
+    else if (i == list.length - 1) { //mengecek jika seluruh kursi sudah dicek
         // tambah penumpang di akhir array
         list.push(passengerName);
         // kembalikan isi array & keluar dari function
@@ -49,16 +47,39 @@ let addPassenger = function(passengerName, list){
 }
 }
 }
-let continueAdding = true;
-while (continueAdding){
-    let passengerName = prompt('Masukkan nama penumpang: \n');
-    if (passengerName == null || passengerName.trim() === ""){
-        alert('nama tidak boleh kosong !');
-    } else {
-        addPassenger(passengerName, list);
-    }
-    continueAdding = confirm('tambah?');
-}
+// let continueAdding = true;
+// while (continueAdding){
+//     let passengerName = prompt('Masukkan nama penumpang: ');
+//     if (passengerName == null || passengerName.trim() === ""){
+//         alert('nama tidak boleh kosong !');
+//     } else {
+//         addPassenger(passengerName, list);
+//     }
+//     continueAdding = confirm('tambah?');
+// }
 
 // console.log('input dari prompt: ', inputName);
-console.log('hasil akhir: ', list);
+// console.log('hasil akhir: ', list);
+
+// 1. deletePassenger()
+// 1.1 namaPenumpang
+// 1.2 array penumpang
+// Rules:
+// a. jika angkot kosong, tampilkan pesan bahwa angkot kosong
+// b jika ada penumpang yang namanya sesuai, hapus nama penumpang pada array dengan memberi nilai undefined
+// c. jika tidak ada penumpang yang namanya sesuai, tampilkan pesan kesalahannya
+let deletePassenger = function (passengerName, list){
+    if (list.length == 0){
+        console.log('masih belum ada penumpang.');
+    } else {
+        for (let i =0; i < list.length; i++){
+            if(list[i] == passengerName){
+                list [i] = undefined;
+                return list;
+            } else if (i == list.length -1){
+                console.log(passengerName+' tidak ada di dalam list');
+                return list;
+            }
+        }
+    }
+}
