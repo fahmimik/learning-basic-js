c5cb653f6ddf8b1dc5635c5da48516007ad4a4f0
fahmimik <fahmimik22@gmail.com>
2025-10-22 17:12:15 +0700

    exercise 2

---
+++ vid44ExcerciseTwo.js
@@ -0,0 +1,12 @@
+// have driver data
+// have route data
+class Driver {
+    constructor(id, name, route){
+        this.id = id;
+        this.name = name;
+        this.route = route;
+    }
+    showed(){
+        console.log()
+    }
+}
